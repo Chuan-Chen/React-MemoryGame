33ae0960cc783419409a978c8e25ec855bfaaa61
Chuan Chen (Shawn) <61473858+Chuan-Chen@users.noreply.github.com>
2022-09-27 14:00:00 -0400

    updates

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Tile from "./Tile";
+import Tiles from "./Tiles.json"
 import "./Game.css"
 
-export default function Game(){
-
+export default function Game(props){
+    const tiles = [];
+    for(let i = 0; i < 9; i++){
+        tiles.push(<Tile/>);
+    }
     return(
         <div className = "gameContainer">
-            <Tile/>
-            {() => {
-                for(let i = 0; i < 9; i++){
-                    <Tile/>
-                }
-            }}
+            {tiles.map((item) => {
+                return item
+            })}
         </div>
     );
 }
