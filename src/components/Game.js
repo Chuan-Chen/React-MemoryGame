b975a83bbdb1c4e36c76cdcce6d7e6b700120db2
Chuan Chen (Shawn) <61473858+Chuan-Chen@users.noreply.github.com>
2022-09-28 15:39:27 -0400

    finished and made production build

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Score from "./Score";
 import Tile from "./Tile";
 import tiles from "./Tiles.json"
@@ -7,25 +7,72 @@ import "./Game.css"
 export default function Game(props){
     const [score, setScore] = useState(0);
     const [hScore, sethScore] = useState(0);
+    const [reset, setReset] = useState(false);
+    const [Tiles, setTiles] = useState(["🤩", "🤪", "🤭", "🤫", "🤮", "🤯", "🤬", "🛸", "₿" ]);
 
-    function clickHandler(e){
-        setScore(score + 1);
-        sethScore(score + 1);
+    useEffect(()=>{
+        if(score === 9){
+            setScore(0);
+            setReset(true);
+            console.log("useeffect in game");
+        }
+       
+    },[score]);
+
+    function shuffle(stuff){
+        let array = [...stuff];
+        //not my algo
+        let currentIndex = array.length,  randomIndex;
+        while (currentIndex != 0) {
+
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+        return array;
 
     }
+
+    function resetGame(){
+        //after game has reset it needs to change back to default state or else it reset will not work any more
+        //this is called after game has reached 9 or if reset==true;
+        setReset(false);
+    }
+    
+
+    function clickHandler(e, hasClicked){
+        if(score === 9){
+
+        }else if(hasClicked){
+            setScore(0);
+            setReset(true);
+            setTiles(shuffle(Tiles));
+        }else{
+            setScore(score + 1);
+            setTiles(shuffle(Tiles));
+            if(hScore <= score){
+                sethScore(score + 1);
+            }
+        }
+        
+    }
+
     return(
+       
         <div className = "container">
             <div className = "scoreboard">
                 <Score score={score} name = "Score"/>
                 <Score score={hScore} name = "High Score"/>
             </div>
-            
-            <div className = "gameContainer">
+            <div className = "center">
+                <div className = "gameContainer">
+                
                 {
-                    tiles.tiles.map((item, i) => {
-                        return <Tile onClick={clickHandler} name = {item.name} key = {i} value = {0}/>
-                    })
-                }
+                    Tiles.map((item, i)=>{return <Tile resetGame ={resetGame} onClickHandler={clickHandler} name={item} key={i} value={reset} hScore={hScore}/>})
+                }   
+                
+                
+                </div>
             </div>
         </div>
 
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Tile.css";
 
 
 export default function Tile(props){
-    const [click, setClick] = useState(0)
+    const [click, setClick] = useState(false);
+
+
+    useEffect(()=>{
+        if(props.hScore === 9 || props.value){
+            setClick(false);
+            props.resetGame;
+        }
+        console.log("useeffect in tile")
+    }, [props.value]);
+
+    function clickHandler(e){
+        console.log("clicked")
+        props.onClickHandler(e, click); 
+        setClick(!click);
+    }
     return(
-        <div onClick = {() => {props.onClick; setClick(!click)}} className = "tile">
+        
+        <div onClick = {clickHandler} className = "tile">
             {props.name}
         </div>
     );
